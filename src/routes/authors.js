04755b3f59c277e2095d5f67fdf8bd571a8a1c46
orky161 <orky161@gmail.com>
2020-11-27 23:57:42 +0200

    add auth

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,14 +1,15 @@
 require("express-async-errors")
 const express = require('express')
 const authorsLogic = new (require("../bl/authors"))()
+const {permission_mw, auth_mw, equalId_mw} = require("../auth")
 
 const router = express.Router()
 
-router.get("/",(async (req, res) => {
+router.get("/", (async (req, res) => {
     const authors = await authorsLogic.getAuthors()
     res.status(200).json({authors})
 }))
-router.post("/",(async (req, res) => {
+router.post("/", [auth_mw, permission_mw("ADMIN")],(async (req, res) => {
     const author = await authorsLogic.addAuthor(req.body)
     res.status(200).json({author})
 }))
@@ -16,11 +17,11 @@ router.get("/:id",(async (req, res) => {
     const author = await authorsLogic.getAuthor(req.params.id)
     res.status(200).json({author})
 }))
-router.put("/:id",(async (req, res) => {
+router.put("/:id", [auth_mw, permission_mw("ADMIN")],(async (req, res) => {
     await authorsLogic.updateAuthor(req.params.id, req.body)
     res.status(202).json({message: "success"})
 }))
-router.delete("/:id",(async (req, res) => {
+router.delete("/:id", [auth_mw, permission_mw("ADMIN")],(async (req, res) => {
     await authorsLogic.deleteAuthor(req.params.id)
     res.status(202).json({message: "success"})
 }))
